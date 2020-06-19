58b474792e4689130d5f597934a6969183659700
root <root@ip-172-31-6-83.ap-southeast-2.compute.internal>
2020-06-19 04:40:18 +0000

    clone from fsg theme

---
+++ Magento_Theme/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    paths: {
+        'slickslider': "js/slick.min"
+    },
+    shim: {
+        'slickslider': {
+            deps: ['jquery']
+        }
+    },
+    map: {
+        '*': {
+            'checkoutjs': 'IWD_Opc/js/jquery.accordion-wizard'
+        }
+    }
+};
